08c6bc199da73f20a42a524dd7f4344987162405
Eduardo1601 <eduardosalinas2001@gmail.com>
2019-10-09 11:34:06 -0600

    listo

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -5,42 +5,89 @@
       <md-app-toolbar class="md-primary" md-elevation="0">
         <md-button class="md-icon-button" @click="toggleMenu" v-if="!menuVisible">
            <div>
-      <md-icon md-src="/assets/reorder-24px.svg" />
+      <md-button class="md-icon-button md-raised md-accent">
+        <md-icon></md-icon>
+      </md-button>
         </div>
         </md-button>
          <div>
       <md-icon md-src="/assets/reorder-24px.svg" />
         </div>
-        <span class="md-title">My Title</span>
+        <span class="md-title">EsourceCapital</span>
       </md-app-toolbar>
 
-      <md-app-drawer :md-active.sync="menuVisible" md-persistent="mini">
+      <md-app-drawer :md-active.sync="menuVisible" md-persistent="full">
         <md-toolbar class="md-transparent" md-elevation="0">
           <md-list-item>
           <md-avatar>
             <img src="https://placeimg.com/40/40/people/6" alt="People">
           </md-avatar>
-          <span class="md-list-item-text">Mary Johnson</span>
+          <span class="md-list-item-text">Carlos Salinas</span>
         </md-list-item>
 
-          <div class="md-toolbar-section-end">
-            <md-button class="md-icon-button md-dense" @click="toggleMenu">
-            </md-button>
+          <div>
+<md-button @click="toggleMenu" class="md-icon-button md-raised md-accent">
+         <md-icon></md-icon>
+        </md-button>
           </div>
         </md-toolbar>
 
-          <md-list-item>
-            <md-button class="dos" @click="toggleMenu">Perfil</md-button> 
-          </md-list-item>
+          <md-dialog :md-active.sync="Perfil">
+      <md-dialog-title>Preferences</md-dialog-title>
 
-          <md-list-item>
-            <md-button class="uno" @click="toggleMenu">Solicitudes</md-button>
-          </md-list-item>
+      <md-tabs md-dynamic-height>
+        <md-tab md-label="General">
+         <div>
+    <md-card class="md-primary">
+      <md-card-header>
+        <md-card-header-text>
+          <div class="md-title">Carlos Salinas</div>
+          <div class="md-subhead">Desarrollo</div>
+        </md-card-header-text>
+        <md-card-media>
+          <img src="https://placeimg.com/40/40/people/6" alt="Avatar">
+        </md-card-media>
+        </md-card-header>
+        <md-card-actions>
+        <md-button>Editar</md-button>
+      </md-card-actions>
+        </md-card>
+  </div>
+        </md-tab>
 
+        <md-tab md-label="Activity">
+         
+        </md-tab>
+
+        <md-tab md-label="Account">
+         
+        </md-tab>
+      </md-tabs>
+
+      <md-dialog-actions>
+        <md-button class="md-primary" @click="Perfil = false">Close</md-button>
+        <md-button class="md-primary" @click="Perfil = false">Save</md-button>
+      </md-dialog-actions>
+    </md-dialog>
+  <md-list-item>
+    <md-button class="md-primary md-raised" @click="Perfil = true">Perfil</md-button>
+  </md-list-item>
+      <md-list-item>
+             <md-button class="md-raised md-primary">Solicitudes</md-button>
+  </md-list-item>
         </md-list>
       </md-app-drawer>
 
       <md-app-content>
+        <md-field>
+      <label>Descripcióon</label>
+      <md-input v-model="withLabel" placeholder="A nice placeholder"></md-input>
+    </md-field>
+    <md-button class="md-icon-button md-list-action">
+            <md-icon class="md-primary"></md-icon>
+          </md-button>
+           <md-button class="md-raised md-accent">Denegado</md-button>
+      <md-button class="md-raised md-primary">Aceptado</md-button>
       </md-app-content>
     </md-app>
   </div>
@@ -50,6 +97,8 @@
   export default {
     name: 'HelloWorld',
     data: () => ({
+      Perfil: false,
+      withLabel: null,
       // eslint-disable-next-line 
       menuVisible: false
     }),// eslint-disable-next-line 
@@ -67,6 +116,9 @@
 </script>
 
 <style lang="scss" scoped>
+  .md-dialog {
+    max-width: 768px;
+  }
   .md-app {
     min-height: 350px;
     border: 1px solid rgba(#000, .12);
